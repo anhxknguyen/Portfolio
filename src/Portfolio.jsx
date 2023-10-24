ea08334c555f9a3dde051bc6cf58b62cf9f80eca
Bon Nguyen <115838607+anhxknguyen@users.noreply.github.com>
2023-10-24 04:14:26 -0400

    Renders the portfolio by page components.

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,24 +1,11 @@
 import "./styles/index.css";
 import React from "react";
-import Navbar from "./Components/Navbar";
-import Intro from "./Components/Intro";
-import Contact from "./Components/Contact";
+import Home from "./Components/Home";
 
 function Portfolio() {
   return (
-    <div
-      className="flex flex-col justify-between items-start font-neueBook 
-    p-0 h-ninetyFiveVH w-{9/10} mt-6 mx-6 border-solid border-2 border-darkGray px-10 py-10 gap-0 animate-fadeInFast"
-    >
-      <div className="animate-fadeInRightMed">
-        <Intro />
-      </div>
-      <div className="self-end animate-fadeInLeftSlow">
-        <Navbar />
-      </div>
-      <div className="animate-fadeInRightMed">
-        <Contact />
-      </div>
+    <div>
+      <Home />
     </div>
   );
 }
