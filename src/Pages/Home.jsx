e70903868c309b2ef3439bb414b2241a9f0da241
Bon Nguyen <115838607+anhxknguyen@users.noreply.github.com>
2023-10-25 00:16:11 -0400

    trial and error mobile 2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Intro from "../Components/Intro";
 import Contact from "../Components/Contact";
-import { motion } from "framer-motion";
 
 const Home = ({ setPage }) => {
   return (
     <div
       className="flex flex-col justify-between items-start font-neueBook 
-     h-full w-{9/10} animate-fadeInFast"
+     h-full w-full animate-fadeInFast"
     >
       <div className="animate-fadeInRightMed">
         <Intro />
       </div>
-      <div className="self-end animate-fadeInLeftSlow ">
+      <div className="self-end animate-fadeInLeftSlow">
         <Navbar setPage={setPage} />
       </div>
       <div className="animate-fadeInRightMed">
