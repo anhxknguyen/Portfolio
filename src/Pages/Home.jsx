48855a4c9404fc8c2d5d1874fae3a14993bbfab5
Bon Nguyen <115838607+anhxknguyen@users.noreply.github.com>
2023-10-27 05:33:31 -0400

    added monkey!

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Components/Navbar";
 import Intro from "../Components/Intro";
 import Contact from "../Components/Contact";
 import { motion } from "framer-motion";
+import monkey from "../images/icon.png";
 
 const Home = () => {
   return (
     <motion.div className="flex flex-col justify-between items-start font-neueBook h-full w-full">
+      <motion.img
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 0.2,
+          transition: { delay: 0.6, duration: 0.5 },
+        }}
+        exit={{ opacity: 0, transition: { duration: 0.25 } }}
+        src={monkey}
+        alt="icon"
+        className="fixed w-1/2 sm:w-40 md:w-30 lg:w-1/5 left-1/2 top-1/2 opacity-20 transform -translate-x-1/2 -translate-y-1/2"
+      />
       <motion.div
         initial={{
           opacity: 0,
@@ -15,7 +29,7 @@ const Home = () => {
         animate={{
           opacity: 1,
           transform: "translateX(0px)",
-          transition: { delay: 0, duration: 0.25 },
+          transition: { delay: 0, duration: 0.5 },
         }}
         exit={{ opacity: 0, transition: { duration: 0.25 } }}
       >
@@ -30,7 +44,7 @@ const Home = () => {
         animate={{
           opacity: 1,
           transform: "translateX(0px)",
-          transition: { delay: 0.2, duration: 0.25 },
+          transition: { delay: 0.2, duration: 0.5 },
         }}
         exit={{ opacity: 0, transition: { duration: 0.25 } }}
         className="self-end"
@@ -45,7 +59,7 @@ const Home = () => {
         animate={{
           opacity: 1,
           transform: "translateX(0px)",
-          transition: { delay: 0.4, duration: 0.25 },
+          transition: { delay: 0.4, duration: 0.5 },
         }}
         exit={{ opacity: 0, transition: { duration: 0.25 } }}
       >
