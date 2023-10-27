4d893b57e4b2aad825fbb65d805051c9ce92b0dc
Bon Nguyen <115838607+anhxknguyen@users.noreply.github.com>
2023-10-27 01:15:56 -0400

    hash routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Portfolio from "./Portfolio.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
